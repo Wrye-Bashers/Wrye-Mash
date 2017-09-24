4e330c91dcbadbfd4d5374150fc470cfe6fb432e
Sharlikran <danielhmpdx16@gmail.com>
2017-09-23 23:13:46 -0700

    Fix for bolt.Path.getNorm on Python 2.7.8
    Reference Commit:
    
    https://github.com/wrye-bash/wrye-bash/commit/73ec883d7fae7735191389cbbe851098147b4be2

--- Mopy/bolt.py
+++ Mopy/bolt.py
@@ -247,12 +247,9 @@ class Path(object):
     @staticmethod
     def getNorm(name):
         """Return the normpath for specified name/path object."""
-        if not name:
-            return name
-        elif isinstance(name, Path):
-            return name._s
-        else:
-            return os.path.normpath(name)
+        if isinstance(name, Path): return name._s
+        elif not name: return name
+        else: return os.path.normpath(name)
 
     @staticmethod
     def getCase(name):
