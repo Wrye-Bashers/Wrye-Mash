d57db0602658116021367dd6bba9940d0f95ad60
Sharlikran <danielhmpdx16@gmail.com>
2017-09-29 14:25:34 -0700

    Style changes to tes3cmd gui

--- Mopy/mash/tes3cmd/gui.py
+++ Mopy/mash/tes3cmd/gui.py
@@ -12,9 +12,10 @@ class OutputParserMixin:
     Thi is a mixin to allow unit testing, as we cannot initiate Cleaner
     without a lot of faf with wx
     """
+
     def ParseOutput(self, output):
         """ Parses the output for a single mod """
-        stats   = ''
+        stats = ''
         cleaned = ''
         inStats = False
 
@@ -31,23 +32,27 @@ class OutputParserMixin:
         return stats, cleaned
 
 
-class CleanOp( tes3cmdgui.cleanop ):
-	def __init__( self, parent ):
-		tes3cmdgui.cleanop.__init__( self, parent )
-	
-	def OnCancel( self, event ):
-		pass
-	
-	def OnCleanClick( self, event ):
-		pass
+class CleanOp(tes3cmdgui.cleanop):
+    def __init__(self, parent):
+        tes3cmdgui.cleanop.__init__(self, parent)
+
+    def OnCancel(self, event):
+        pass
+
+    def OnCleanClick(self, event):
+        pass
+
 
 DONE_HEADER, DONE_CLEAN = range(2)
 
-#the thread that manages the threaded process uses wx events
-#to post messsages to the main thread
+# the thread that manages the threaded process uses wx events
+# to post messsages to the main thread
 EVT_DONE_ID = wx.NewId()
+
+
 def EVT_DONE(win, func):
     win.Connect(-1, -1, EVT_DONE_ID, func)
+
 
 class DoneEvent(wx.PyEvent):
     def __init__(self, t):
@@ -67,7 +72,7 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
     called 
     """
 
-    def __init__(self, parent, files, args = {}):
+    def __init__(self, parent, files, args={}):
         """
         parent - Parent window
         files - list of files to clean
@@ -82,9 +87,9 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
 
         self.output = {}
 
-        EVT_DONE(self,self.OnDone)
+        EVT_DONE(self, self.OnDone)
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     def Start(self, callback=None):
         """
         Starts running tes3cmd over all the files
@@ -115,12 +120,12 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
         self.currentFile = filename = self.remainingFiles.pop()
         lowerFname = filename.lower()
 
-        #we don't want to clean morrowind.esm
+        # we don't want to clean morrowind.esm
         if lowerFname == 'morrowind.esm':
             self.StartNext()
             return
 
-        #if we copy expansions, don't clean gmsts (I think)
+        # if we copy expansions, don't clean gmsts (I think)
         args = copy(self.args)
         if lowerFname == 'tribunal.esm' or lowerFname == 'bloodmon.esm':
             args['gmsts'] = False
@@ -128,12 +133,12 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
 
         self.mCurrentFile.SetLabel(filename)
 
-        #start cleaning the current file
+        # start cleaning the current file
         func = lambda: wx.PostEvent(self, DoneEvent(DONE_CLEAN))
         self.cleaner = tes3cmd.Threaded(callback=func)
         self.cleaner.clean([filename], **args)
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # GUI clean event and event handlers
     def OnDone(self, event):
         """
@@ -144,19 +149,19 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
             self.DoneHeader()
         elif event.doneType == DONE_CLEAN:
             self.DoneClean()
-            
+
     def DoneClean(self):
         """
         When the file has done cleaning, we then sync the headers
         """
         out = self.cleaner.out
         err = self.cleaner.err
-        
+
         stats, cleaned = self.ParseOutput(out)
-        self.output[self.currentFile] = { 'stats' : stats,
-                                          'cleaned' : cleaned,
-                                          'output' : out,
-                                          'error' : err }
+        self.output[self.currentFile] = {'stats': stats,
+            'cleaned'                           : cleaned,
+            'output'                            : out,
+            'error'                             : err}
 
         func = lambda: wx.PostEvent(self, DoneEvent(DONE_HEADER))
         self.syncer = tes3cmd.Threaded(callback=func)
@@ -175,13 +180,13 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
             self.Select(self.currentFile)
 
         tfLen = len(self.files)
-        ratio = (tfLen - len(self.remainingFiles))/float(tfLen)
-        self.mProgress.SetValue(ratio*100)
+        ratio = (tfLen - len(self.remainingFiles)) / float(tfLen)
+        self.mProgress.SetValue(ratio * 100)
 
         self.mCurrentFile.SetLabel('')
         self.StartNext()
-	
-    #--------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
     # GUI  skip and stop events
     def OnSkip(self, event):
         """ When the skip button is pressed """
@@ -196,7 +201,7 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
 
         self.mCurrentFile.SetLabel('')
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # GUI list selection events to view the logs for a file
     def OnSelect(self, event):
         """ ListBox select, selecting a mod to view the stats of """
@@ -209,7 +214,7 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
         self.mLog.SetValue(item['cleaned'])
         self.mErrors.SetValue(item['error'])
 
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # Log functions and save log button event
     def GetLog(self, fileName):
         """ Gets the log text for the given file name """
@@ -224,16 +229,15 @@ class Cleaner(tes3cmdgui.cleaner, OutputParserMixin):
     def SaveLog(self, fileName):
         """ Saves the log information to the given location """
         f = open(fileName, 'w')
-        for fn in self.output.keys(): 
+        for fn in self.output.keys():
             f.write('--' + fn + '--\n')
             f.write(self.GetLog(fn))
         f.close()
 
     def OnSaveLog(self, event):
         dlg = wx.FileDialog(self, 'Save log', os.getcwd(),
-                            'tes3cmd.log', '*.log',
-                            wx.SAVE | wx.OVERWRITE_PROMPT)
+            'tes3cmd.log', '*.log',
+            wx.SAVE | wx.OVERWRITE_PROMPT)
         if dlg.ShowModal() == wx.ID_OK:
             fileName = os.path.join(dlg.GetDirectory(), dlg.GetFilename())
             self.SaveLog(fileName)
-
