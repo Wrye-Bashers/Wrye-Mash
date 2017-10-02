183e7db2284b3707635eb777a9eb2874dab7061b
Jacob Essex <jacob@jacobessex.com>
2017-10-01 20:00:57 -0700

    Improved mod reordering code

--- Mopy/masher.py
+++ Mopy/masher.py
@@ -1133,18 +1133,14 @@ class ModList(gui.List):
         event.Skip()
         self.moveSelected(
             event,
-            lambda x: x - 1,
-            lambda x: x - 1,
-            lambda l: l.pop(0)
+            (-1)
         )
 
     def OnDownPress(self, event):
         event.Skip()
         self.moveSelected(
             event,
-            lambda x: x + 1,
-            lambda x: x + 1,
-            lambda l: l.pop()
+            (+1)
         )
 
     def OnSpacePress(self, event):
@@ -1152,8 +1148,14 @@ class ModList(gui.List):
             self.ToggleModActivation(fileName)
         self.Refresh()
 
-    def moveSelected(self, event, relationFunc, timeFunc, getSelectedFunc):
-        """Moves selected files up or down
+    def moveSelected(self, event, moveMod):
+        """
+        New Method:
+        Moves selected files up or down (depending on moveMod)
+        Changed in: Improved mod reordering code
+
+        Old Method:
+        Moves selected files up or down
 
         event -- the event that caused the need for movement
 
@@ -1170,22 +1172,6 @@ class ModList(gui.List):
                            from the list
         """
 
-        def alterModTimeIfReq(movingToIndex, movingToTime):
-            """Checks if there is something (another mod) in the way of where we want to move this mod
-               to. If there is it moves it in the same direction, again calling this function to check if
-               anything is in the way.
-            """
-            # check if something is in the way
-            modInWayIndex = relationFunc(movingToIndex)
-            if hasItemAtIndex(modInWayIndex):
-                mod = mosh.modInfos[items[modInWayIndex]]
-                # if it has exaclt the same time, then we need to move it.
-                if mod.mtime == movingToTime:
-                    newTime = timeFunc(mod.mtime)
-                    # check we aren't moving it onto something
-                    alterModTimeIfReq(modInWayIndex, newTime)
-                    mosh.modInfos[items[modInWayIndex]].setMTime(newTime)
-
         if not event.ControlDown():
             return
 
@@ -1197,39 +1183,37 @@ class ModList(gui.List):
         if not selected:
             return
 
-        # shallow copy of the selected files that need processing
-        process = list(selected)
-        process.sort(key=lambda x: mosh.modInfos[x].mtime)
-
-        items = self.GetItems()
-        while process:
-            items.sort(key=lambda x: mosh.modInfos[x].mtime)
-
-            selFileName = getSelectedFunc(
-                process)  # this reduces the process list
-            selFileIndex = items.index(selFileName)
-            selFileTime = mosh.modInfos[selFileName].mtime
-            newSelFileTime = timeFunc(
-                selFileTime);  # default. This is changed later if requried
-            movePastFileIndex = relationFunc(selFileIndex)
-
-            hasItemAtIndex = lambda x: x >= 0 and x < len(items)
-
-            if hasItemAtIndex(movePastFileIndex):
-                movePastFileName = items[movePastFileIndex]
-                movePastFileTime = mosh.modInfos[movePastFileName].mtime
-                newSelFileTime = timeFunc(movePastFileTime);
-
-                alterModTimeIfReq(movePastFileIndex, newSelFileTime)
-
-            mosh.modInfos[selFileName].setMTime(newSelFileTime)
-            mosh.modInfos.refreshDoubleTime()
-
-        # ensure correct items (i.e the ones we started with) are selected
-        self.ClearSelected()
-        self.SelectItems(selected)
+        self.moveSelectedFilter(selected, moveMod,
+            lambda x: x.lower().endswith('esp'))
+        self.moveSelectedFilter(selected, moveMod,
+            lambda x: x.lower().endswith('esm'))
 
         self.Refresh()
+
+    def moveSelectedFilter(self, origSel, moveMod, pred):
+        selected = [x for x in origSel if pred(x)]
+        selected.sort(key=lambda x: mosh.modInfos[x].mtime,
+            reverse=(moveMod != -1))
+
+        items = [x for x in self.GetItems() if pred(x)]
+        items.sort(key=lambda x: mosh.modInfos[x].mtime)
+        for item in selected:
+            pos = items.index(item)
+            movePos = pos + moveMod
+            if movePos < 0 or movePos >= len(items):
+                break
+            items[pos], items[movePos] = items[movePos], items[pos]
+
+        # correct the times on the list
+        getTime = lambda x: mosh.modInfos[x].mtime
+        for i in range(len(items) - 1, 0, -1):
+            if getTime(items[i]) <= getTime(items[i - 1]):
+                mosh.modInfos[items[i - 1]].setMTime(getTime(items[i]) - 1)
+
+        mosh.modInfos.refreshDoubleTime()
+
+        self.ClearSelected()
+        self.SelectItems(origSel)
 
 
 # ------------------------------------------------------------------------------
