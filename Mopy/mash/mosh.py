3887f274f84dccb8fd4d94ffc5acfd720433e607
Sharlikran <danielhmpdx16@gmail.com>
2017-10-05 08:13:35 -0700

    [Unicode] class ScreensData

--- Mopy/mash/mosh.py
+++ Mopy/mash/mosh.py
@@ -3624,7 +3624,7 @@ class ScreensData(DataDict):
         if ssBase.head:
             self.dir = self.dir.join(ssBase.head)
         newData = {}
-        reImageExt = re.compile(r'\.(bmp|jpg)$', re.I)
+        reImageExt = re.compile(ur'\.(bmp|jpg)$',re.I|re.U)
         # --Loop over files in directory
         for fileName in self.dir.list():
             filePath = self.dir.join(fileName)
